70b5e5fd1269ff8d3c50796278aa0bf605606ce5
Limour <93720049+Limour-dev@users.noreply.github.com>
2024-02-12 09:09:12 +0800

    Add files via upload

--- mods/btn_submit_vo_suggest.py
+++ mods/btn_submit_vo_suggest.py
@@ -1,16 +1,13 @@
 def init(cfg):
-    cfg['btn_submit_vo_suggest'].click(
-        **cfg['btn_start']
-    ).success(
-        **cfg['btn_submit_fn_usr']
-    ).success(
-        **cfg['btn_rag_fn']
-    ).success(
-        **cfg['btn_submit_fn_bot']
-    ).success(
-        **cfg['btn_vo_fn']
-    ).success(
-        **cfg['btn_suggest_fn']
-    ).success(
-        **cfg['btn_finish']
-    )
+    # ========== 自定义组合建的功能 ==========
+    combine = [
+        'btn_submit_fn_usr',
+        'btn_rag_fn',
+        'btn_submit_fn_bot',
+        'btn_vo_fn',
+        'btn_suggest_fn'
+    ]
+    tmp = cfg['btn_submit_vo_suggest'].click(**cfg['btn_start'])
+    for f in combine:
+        tmp = tmp.success(**cfg[f])
+    tmp.success(**cfg['btn_finish'])
